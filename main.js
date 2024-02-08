edb65b3ef9a9bd591889d03e44b751c817ee1d63
Ethihas2 <103252587+Ethihas2@users.noreply.github.com>
2024-02-08 21:17:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function login(){
+    username = document.getElementById("username").value 
+    password = document.getElementById("password").value 
+
+    if(username == "it's._.shreyy"){
+        if(password == "ILOVEYOU"){
+            window.location.href = 'load.html';
+        }
+    }
+    
+}
+
+
+
+
+
